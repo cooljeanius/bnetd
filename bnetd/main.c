4074e9085c818886c2fde0d6d63ba35a1055c992
System Administrator <root@gl00b05044.cats.gwu.edu>
2013-02-04 04:37:08 -0500

    Initial Commit

---
+++ bnetd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bnetd
+//
+//  Created by System Administrator on 2/4/13.
+//  Copyright (c) 2013 George Washington University. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
