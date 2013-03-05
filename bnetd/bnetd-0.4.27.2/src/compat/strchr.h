ab97e2e72b11404ee7fe83abb06b8af33f3ff9b5
System Administrator <root@gl00b05045.cats.gwu.edu>
2013-03-05 09:21:14 -0500

    more messing around with the source

--- bnetd/bnetd-0.4.27.2/src/compat/strchr.h
+++ bnetd/bnetd-0.4.27.2/src/compat/strchr.h
@@ -15,6 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
 #ifndef INCLUDED_STRCHR_PROTOS
 #define INCLUDED_STRCHR_PROTOS
 
@@ -23,7 +26,7 @@
 #ifdef HAVE_INDEX
 # define strchr index
 #else
-# error "This program requires either strchr() or index()"
+#warning "This program requires either strchr() or index()"
 #endif
 
 #endif
